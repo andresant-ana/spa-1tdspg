09e382e44650e484fd9b273b1c05e891199761ab
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-23 09:50:55 -0300

    Iniciando o projeto Boyler-Plate Vite+React. Components

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
+import Rodape from "./components/Rodape";
+
+
+export default function App() {
+
+  //ÁREA DECLARATIVA
+  let meuNome = "Alexandre";
+
+  return (
+    <>
+      {/* Área Imperativa! */}
+      <div className="container">
+          <Cabecalho nomeDoUsuario={meuNome}>
+            Hello World!!
+          </Cabecalho>
+
+          <Conteudo/>
+
+          <Rodape/>
+
+      </div>
+    </>
+  )
+
+}
