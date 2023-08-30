f3063439460dd92cec50fa6f9ad52e98613b3ddd
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-30 09:54:17 -0300

    Criano rotas com router, utilizando,  createBrowser + RouterProvider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
+import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho";
-import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
-
 
 export default function App() {
 
-  //ÁREA DECLARATIVA
-  let meuNome = "Alexandre";
-
   return (
     <>
-      {/* Área Imperativa! */}
-      <div className="container">
-          <Cabecalho nomeDoUsuario={meuNome}>
-            Hello World!!
-          </Cabecalho>
-
-          <Conteudo/>
-
-          <Rodape/>
-
-      </div>
+        <Cabecalho/>
+        <Outlet/>
+        <Rodape/>
     </>
   )
 
