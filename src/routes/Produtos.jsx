6b475a6c83db77c606b36ca6a1df10de85731fe7
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-01 11:16:53 -0300

    React Icons adicionado

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
+import {AiOutlineEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import "./Produtos.css";
 
 export default function Produtos() {
@@ -15,7 +16,7 @@ export default function Produtos() {
                     <th>ID</th>
                     <th>NOME</th>
                     <th>PREÇO</th>
-                    <th>EDITAR/EXCLUIR</th>
+                    <th><Editar/> / <Excluir/></th>
 
                 </tr>
             
@@ -24,7 +25,7 @@ export default function Produtos() {
                             <td>{produto.id}</td>
                             <td>{produto.nome}</td>
                             <td>{produto.preco}</td>
-                            <td> <Link to={`/editar/produto/${produto.id}`}>EDITAR</Link> / <Link to={`/excluir/produto/${produto.id}`}>EXCLUIR</Link> </td>
+                            <td> <Link to={`/editar/produto/${produto.id}`}><Editar/></Link> / <Link to={`/excluir/produto/${produto.id}`}><Excluir/></Link> </td>
                         </tr>
                 ))}
             </table>
