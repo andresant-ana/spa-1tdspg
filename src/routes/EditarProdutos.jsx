df566399d41151432aed49e31271baa9bac9873d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-01 11:30:25 -0300

    Recuperando o produto para editar.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { ListaProdutos } from "../components/ListaProdutos";
 
 export default function EditarProdutos() {
 
@@ -7,11 +8,19 @@ export default function EditarProdutos() {
   //Recuperando o id do produto com o HOOK useParam();
   const {id} = useParams();
 
+    //Utilizando o filter na Lista de Produtos para recuperar um produto através do id como parâmetro.
+    const prodRecuperadoPorId = ListaProdutos.filter( (produto) => {
+        if(produto.id == id){
+          return produto;
+        }
+    });
+
   return (
     <>
         <div>
             <h1>EDITAR-PRODUTOS</h1>
-            <p>Produto selecionado - {id}</p>
+            <p>NOME: - {prodRecuperadoPorId[0].nome}</p>
+            <p>PREÇO: - {prodRecuperadoPorId[0].preco}</p>
         </div>
     </>
   )
