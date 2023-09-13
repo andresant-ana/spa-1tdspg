2bb4970b67fb073a26bf7f3fdc3d54e6b5d25ba8
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 08:45:17 -0300

    Editar realizado com useState/useNavigate, destructuring.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,10 +1,12 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import { useState } from "react";
 
 export default function EditarProdutos() {
 
   document.title = "Editar Produtos";
+
+  const navigate = useNavigate();
 
   //Recuperando o id do produto com o HOOK useParam();
   const {id} = useParams();
@@ -20,9 +22,38 @@ export default function EditarProdutos() {
       img: prodRecuperadoPorId[0].img
     });
 
-    const handlerEditarProduto = (event) => {
-        event.preventDefault();
+    const handleChange = (event) => {
+        // console.log(event.target);
+
+        //Destructuring
+        const {name,value} = event.target;
+        //Inserir os dado no objeto produto através do setProduto({...});
+        setProduto({...produto,[name]:value});
+
     }
+
+    const handleSubmit = (event)=>{
+      
+      event.preventDefault();
+
+      //Índice que será utilizado para a sobreposição do produtos na lista.
+      let indice;
+
+      //Localização do índice na lista.
+      ListaProdutos.forEach((item,index)=>{
+        if(item.id == produto.id){
+          indice = index;
+        }       
+      });
+
+      //Utilizando o método splice para alterar o produto no índice especificado.
+      ListaProdutos.splice(indice,1,produto);
+      alert("Produto alterado!");
+
+      navigate("/produtos")
+
+    }
+
 
   return (
     <>
@@ -30,16 +61,20 @@ export default function EditarProdutos() {
 
 
             <h1>EDITAR-PRODUTOS</h1>
-            <form action="#" method="get">
+            <form onSubmit={handleSubmit}>
               <fieldset>
                 <legend>EDITAR PRODUTO</legend>
                 <div>
                   <label htmlFor="idProduto">Nome Produto:</label>
-                  <input type="text" name="nome" id="idProduto" defaultValue={produto.nome} onChange={handlerEditarProduto} />
+                  <input type="text" name="nome" id="idProduto" value={produto.nome} onChange={handleChange} />
                 </div>
                 <div>
-                  <label htmlFor="idPreco">Nome Produto:</label>
-                  <input type="text" name="nome" id="idProduto" defaultValue={produto.nome} onChange={handlerEditarProduto} />
+                  <label htmlFor="idPreco">Preço Produto:</label>
+                  <input type="text" name="preco" id="idPreco" value={produto.preco} onChange={handleChange} />
+                </div>
+                <div>
+                  <label htmlFor="idDesc">Descrição Produto:</label>
+                  <input type="text" name="desc" id="idDesc" value={produto.desc} onChange={handleChange} />
                 </div>
 
                 <div>
@@ -48,6 +83,13 @@ export default function EditarProdutos() {
               </fieldset>
             </form>
         </div>
+
+        <div>
+          <p>Nome : {produto.nome}</p>
+          <p>Preço : {produto.preco}</p>
+          <p>Desc : {produto.desc}</p>
+        </div>
+
     </>
   )
 }
