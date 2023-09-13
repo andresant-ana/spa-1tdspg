07cb95f80d007784182d0bb244559b207158c2f2
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-13 09:52:56 -0300

    Iniciando com useEffect

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -2,16 +2,29 @@ import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
 import {AiOutlineEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import classes from "./Produtos.module.css";
+import { useEffect, useState } from "react";
 
 
 export default function Produtos() {
+
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("useEffect que renderiza sempre!");
+    });
 
     return (
     <>
         <div>
             <h1>PRODUTOS</h1>
 
+                <div>
+                    <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+                </div>
+
             <table className={classes.tabelaProd}>
+
+                <thead>
                 <tr>
                     <th className={classes.tabelaCabecalho}>ID</th>
                     <th className={classes.tabelaCabecalho}>NOME</th>
@@ -19,9 +32,9 @@ export default function Produtos() {
                     <th className={classes.tabelaCabecalho}>PREÇO</th>
                     <th className={classes.tabelaCabecalho}>IMG</th>
                     <th className={classes.tabelaCabecalho}><Editar/> / <Excluir/></th>
-
                 </tr>
-            
+                </thead>
+                <tbody>
                 {ListaProdutos.map( (produto, indice)=>(
 
 <tr className={classes.tabelaLinha} key={indice}  >
@@ -34,6 +47,12 @@ export default function Produtos() {
                             <td className={classes.tabelaDados}> <Link to={`/editar/produto/${produto.id}`}><Editar/></Link> / <Link to={`/excluir/produto/${produto.id}`}><Excluir/></Link> </td>
                         </tr>
                 ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={6}>PRODUTOS</td>
+                    </tr>
+                </tfoot>
             </table>
 
         </div>
