a8a989a924a3e0b4a62c75a292e67c7f4048c19a
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-09-22 10:30:10 -0300

    Refatorando o componente produtos.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,49 +1,23 @@
 import { Link } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
-import {
-  AiOutlineEdit as Editar,
-  AiOutlineDelete as Excluir,
-} from "react-icons/ai";
+import { AiOutlineEdit as Editar, AiOutlineDelete as Excluir} from "react-icons/ai";
 import classes from "./Produtos.module.css";
 import { useEffect, useState } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const [counter, setCounter] = useState(0);
   const [novaListaProdutos, setNovaListaProdutos] = useState([{}]);
-  const [counter2, setCounter2] = useState(0);
-
-  useEffect(() => {
-    console.log("useEffect que renderiza sempre!");
-  });
 
   useEffect(() => {
     setNovaListaProdutos(ListaProdutos);
     console.log("useEffect que renderiza apenas uma vez!");
   }, []);
 
-  useEffect(() => {
-    console.log(
-      "useEffect que renderiza apenas se o objeto/elemento/constante que está sendo monitorado no array de dependências sofrer atualização."
-    );
-  }, [counter2]);
-
   return (
     <>
       <div>
         <h1>PRODUTOS</h1>
-
-        <div>
-          <button onClick={() => setCounter(counter + 1)}>
-            COUNTER - {counter}
-          </button>
-        </div>
-        <div>
-          <button onClick={() => setCounter2(counter2 + 1)}>
-            COUNTER2 - {counter2}
-          </button>
-        </div>
 
         <table className={classes.tabelaProd}>
           <thead className={classes.tabelaCabecalho}>
@@ -87,7 +61,9 @@ export default function Produtos() {
           </tbody>
           <tfoot className={classes.tabelaRodape}>
             <tr>
-              <td colSpan={6}>PRODUTOS INFORMÁTICOS - QTD = {novaListaProdutos.length}</td>
+              <td colSpan={6}>
+                PRODUTOS INFORMÁTICOS - QTD = {novaListaProdutos.length}
+              </td>
             </tr>
           </tfoot>
         </table>
