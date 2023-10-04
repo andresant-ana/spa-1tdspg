18569b0a61c3c611aa098f1a96dcd43a9e97bfad
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-04 08:58:54 -0300

    Refatorando o projeto retirando itens do boyler-plate. Adicionando imagens estáticas.

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -5,7 +5,7 @@ export default function Cabecalho(){
     return(
         <>
             <header className="cabecalho">
-                <h1>Vite + React</h1>
+                
                 <ul>
                     <li><Link to="/">HOME</Link></li>
                     <li><Link to="/produtos">PRODUTOS</Link></li>
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,15 @@
 export default function Home(){
-
+    document.title = "HOME";
     return(
         <>
             <div>
-                <h1>HOME</h1>
+                <h1>Produtos em Oferta....</h1>
+                <figure>
+                    <figcaption>
+
+                    </figcaption>
+                    <img src="/online-shopping_640.png" alt="Produtos diversos" />
+                </figure>
             </div>
         </>
     )
