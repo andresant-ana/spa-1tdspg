2f26a53697a02709883f0009db1c40d8b4a7e751
andresant-ana <rm551575@fiap.com.br>
2023-10-05 21:10:42 -0300

    Estilização da página de Erro 404.

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
 
 export default function Error() {
   return (
     <>
-        <div>
-            <h1>ERRO-404</h1>
-        </div>
+      <div className="cabecalho">
+        <h1>Error 404</h1>
+        <p className="msg">A pagina que voce tentou acessar nao existe</p>
+        <Link to="/" className="btn">Voltar para a página inicial</Link>
+
+      </div>
     </>
   )
 }
